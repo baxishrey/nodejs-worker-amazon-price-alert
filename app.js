b95bcd29e4efc199ace74af5dcfd79e5ebeb865c
Shrey Baxi <baxishrey@gmail.com>
2019-11-06 19:52:09 +0530

    Changed worker implementation

--- app.js
+++ app.js
@@ -3,90 +3,51 @@ const AWS = require('aws-sdk');
 const rp = require('request-promise');
 const $ = require('cheerio');
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+console.log(process.env.NODE_ENV);
+
 AWS.config.update({ region: 'us-east-1' });
-const docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 // CONSTANTS
-const tableName = 'user-price-items';
 const intervalTime = 30000;
 
 // Get username (email) and item id from environment variables
-const email = process.env.email;
-const itemId = process.env.item_id;
+const url = process.env.url;
+const targetPrice = process.env.targetPrice;
 
-if (!email) {
-  console.log('Email is required');
+if (!url) {
+  console.log('URL is required');
   return;
 }
 
-if (!itemId) {
-  console.log('Item ID is required');
+if (!targetPrice) {
+  console.log('Target price is required');
   return;
 }
 
-let trackedItem = undefined;
-
-// Get item from DynamoDB using username and item id
-console.log('Fetching tracked item...');
-getUser(email)
-  .then(
-    data => {
-      const item = data.Item;
-      if (!item) {
-        console.error(`No items found for username ${email}`);
-        return;
-      } else {
-        const tracked_items = Array.from(item.tracked_items);
-        trackedItem = tracked_items.find(ti => ti.id === itemId);
-
-        if (!trackedItem) {
-          console.error(
-            `User ${email} is not tracking the item with id ${itemId}`
-          );
-          return;
-        } else {
-          let interval = setInterval(() => {
-            // Fetch current price of item
-            console.log('Fetching current price...');
-            rp(trackedItem.url, { gzip: true }).then(html => {
-              var price = $(
-                '[id=priceblock_ourprice],[id=priceblock_dealprice]',
-                html
-              )
-                .text()
-                .trim();
-              price = price.replace(/[\u20B9]/g, '').trim();
-              var currentPrice = parseFloat(price.replace(',', ''));
-              console.log(`Current price is ${currentPrice}`);
-              console.log(`Target price is ${trackedItem.targetPrice}`);
-              const hasPriceDropped = currentPrice <= trackedItem.targetPrice;
-              if (hasPriceDropped) {
-                // If price has dropped, send email
-                console.log(
-                  'Price has dropped! Congratulations! Stopping tracking..'
-                );
-                clearInterval(interval);
-              } else {
-                console.log(`Trying again in ${intervalTime / 1000} seconds`);
-              }
-            });
-            // Start interval of 1 hour
-          }, intervalTime);
-        }
-      }
-    },
-    err => {
-      throw new Error(err);
+let interval = setInterval(async () => {
+  // Fetch current price of item
+  console.log('Fetching current price...');
+  try {
+    const html = await rp(url, { gzip: true });
+    var price = $('[id=priceblock_ourprice],[id=priceblock_dealprice]', html)
+      .text()
+      .trim();
+    price = price.replace(/[\u20B9]/g, '').trim();
+    var currentPrice = parseFloat(price.replace(',', ''));
+    console.log(`Current price is ${currentPrice}`);
+    console.log(`Target price is ${targetPrice}`);
+    const hasPriceDropped = currentPrice <= targetPrice;
+    if (hasPriceDropped) {
+      // If price has dropped, send email
+      console.log('Price has dropped! Congratulations! Stopping tracking..');
+      clearInterval(interval);
+    } else {
+      console.log(`Trying again in ${intervalTime / 1000} seconds`);
     }
-  )
-  .catch(err => console.error(err));
-
-function getUser(username) {
-  const params = {
-    TableName: tableName,
-    Key: {
-      userId: username
-    }
-  };
-  return docClient.get(params).promise();
-}
+  } catch (err) {
+    console.log(err);
+  }
+}, intervalTime);
